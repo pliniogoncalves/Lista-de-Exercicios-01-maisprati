61c0ef21046933e80c85d3709ef643e9f3570f2b
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 20:40:20 -0300

    questao 03

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,16 @@
 const prompt = require('prompt-sync')()
+
+let n1 = parseInt(prompt("Digite o Primeiro Numero: "))
+let n2 = parseInt(prompt("Digite o Segundo Numero: "))
+let n3 = parseInt(prompt("Digite o Terceiro Numero: "))
+let n4 = parseInt(prompt("Digite o Quarto Numero: "))
+
+n4 = n1 + n2 + n3
+n1 += 25
+n2 *= 3
+n3 = (n3*12)/100
+
+console.log(n1)
+console.log(n2)
+console.log(n3 + "%")
+console.log(n4)
