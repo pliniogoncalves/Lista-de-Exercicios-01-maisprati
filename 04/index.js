50809092a89cd24edd401671732b19fabc4e3ffd
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 20:46:52 -0300

    questao 04

--- 04/index.js
+++ 04/index.js
@@ -1,1 +1,12 @@
 const prompt = require('prompt-sync')()
+
+let n1 = parseFloat(prompt("Digite a Primeira Nota: "))
+let n2 = parseFloat(prompt("Digite a Segunda Nota: "))
+
+let media = (n1 + n2)/2
+
+if(media>=6){
+    console.log("PARABÉNS! Você foi aprovado!")
+}else{
+    console.log("Você foi reprovado")
+}
