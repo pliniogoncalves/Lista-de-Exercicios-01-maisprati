9b7a1aaba0600729f29bd8076fd630860b66daea
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 22:09:34 -0300

    questao 10 e 11

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,7 @@
 const prompt = require('prompt-sync')()
+
+let n = parseInt(prompt("Digite um numero: "))
+
+for(let i=0;i<10;i++){
+     console.log(n)
+ }
--- 11/index.js
+++ 11/index.js
@@ -1,1 +1,16 @@
 const prompt = require('prompt-sync')()
+
+let valor
+
+do{
+    valor = (parseInt(prompt("Digite um valor: ")))
+
+    if(valor === null || valor <= 0)break;
+    
+    if(valor%2 === 0){
+        console.log("PAR")
+    }else{
+        console.log("IMPAR")
+    }
+
+}while(valor > 0);
