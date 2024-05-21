6726c7f9736131ac6bd1add3fdda70d23d9054ec
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 23:14:07 -0300

    questao 16

--- 16/index.js
+++ 16/index.js
@@ -1,1 +1,23 @@
 const prompt = require('prompt-sync')()
+
+let n = 101
+let contador = 0
+
+while(contador < 50){
+    let divisores = 0
+    let i = 1
+
+    while(i <= n){
+        if(n % i === 0){
+            divisores++;
+        }
+        i++;
+    }
+
+    if(divisores === 2){
+        console.log(n)
+        contador++
+    }
+
+    n++
+}
