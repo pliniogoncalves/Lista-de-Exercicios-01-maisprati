5ab00560265aaf5fdd0c13390a8dda52ed9db718
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 21:20:05 -0300

    questao 07

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,9 @@
 const prompt = require('prompt-sync')()
+
+let macas = parseInt(prompt("Digite o numero de maçãs: "))
+
+if(macas<12){
+    console.log("Valor total da compra = R$"+ macas * 0.3)
+}else{
+    console.log("Valor total da compra = R$"+ macas * 0.25)
+}
