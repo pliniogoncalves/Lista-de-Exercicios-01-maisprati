26e9e3b84dae017e90956b6b1074aab558165bbc
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 22:51:26 -0300

    questao 14

--- 14/index.js
+++ 14/index.js
@@ -1,1 +1,15 @@
 const prompt = require('prompt-sync')()
+
+let n, total=0, contador=0
+
+while (true) {
+
+    n = parseFloat(prompt("Digite o valor do numero: "))
+    if(n === 0)break;
+    
+    total = total + n
+    contador += 1
+     
+}
+
+console.log("A Média é :" + total/contador)
