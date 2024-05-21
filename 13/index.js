4a72f557b1ec34632da786172b8fdfdf232a476c
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 22:23:35 -0300

    questao 13

--- 13/index.js
+++ 13/index.js
@@ -1,1 +1,14 @@
 const prompt = require('prompt-sync')()
+
+let n, contador=5
+
+do{
+    n = parseInt(prompt("Digite o valor de N: "))
+
+    for(let i = 1; i <= n; i++){
+        console.log(n + "x" + i + " = " + n * i)
+    }
+
+    contador--
+}while(contador != 0)
+
