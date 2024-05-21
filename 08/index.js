0cab1c4143f7fcfc5c36e2f64a50f028ec3c004e
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 21:25:42 -0300

    questao 08

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,10 @@
 const prompt = require('prompt-sync')()
+
+let v1 = parseFloat(prompt("Digite o primeiro valor: "))
+let v2 = parseFloat(prompt("Digite o segundo valor: "))
+
+if(v1 > v2){
+    console.log(v2,v1)
+}else{
+    console.log(v1,v2)
+}
