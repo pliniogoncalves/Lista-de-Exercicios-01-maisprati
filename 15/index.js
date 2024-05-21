6d12cf81950f4c9285174089e25d64d0493e86b1
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-20 23:00:10 -0300

    questao 15

--- 15/index.js
+++ 15/index.js
@@ -1,1 +1,17 @@
 const prompt = require('prompt-sync')()
+
+let n, peso, total=0, contador_peso=0
+
+while (true) {
+
+    n = parseFloat(prompt("Digite o valor do numero: "))
+    if(n === 0)break;
+
+    peso = parseFloat(prompt("Digite o valor do peso: "))
+    
+    total = total + n*peso
+    contador_peso += peso
+     
+}
+
+console.log("A Média é :" + total/contador_peso)
